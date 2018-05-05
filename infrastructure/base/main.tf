e448075f65bd5848e9c5a556b358acb66ab24dcd
Gerard Chin <gerardchin91@gmail.com>
2018-05-05 16:14:10 +1200

    change dir struct

--- infrastructure/base/main.tf
+++ infrastructure/base/main.tf
@@ -12,8 +12,20 @@ module "webapp" {
   source  = "AlexanderRMcNeill/secured-static-website/aws"
   version = "1.0.0"
 
-  bucket_name         = "${var.project_name}-webapp"
-  domain_names        = ["${var.project_domain}"]
-  hosted_zone_id      = "${aws_route53_zone.main.zone_id}"
+  bucket_name = "${var.project_name}-webapp"
+
+  domain_names = ["${var.project_domain}"]
+
+  hosted_zone_id = "${aws_route53_zone.main.zone_id}"
+
   acm_certificate_arn = "arn:aws:acm:us-east-1:049552520592:certificate/f877e9ff-863b-4af0-90ce-0831a5cb5c65"
+
+  custom_error_response_objects = [
+    {
+      error_code            = "404"
+      error_caching_min_ttl = 0
+      response_code         = "200"
+      response_page_path    = "/index.html"
+    },
+  ]
 }
